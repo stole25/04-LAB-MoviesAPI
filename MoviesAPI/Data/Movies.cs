10c420563c27a73205ada6120330a3ed7c1bd581
stole25 <119949884+stole25@users.noreply.github.com>
2024-12-10 17:18:37 +0100

    save1

---
+++ MoviesAPI/Data/Movies.cs
@@ -0,0 +1,10 @@
+namespace MoviesAPI.Data;
+
+public class Movies
+{
+    public int Id { get ; set ;}
+    public string Name { get ; set ;}
+    public string Genre { get ; set ;}
+    public int? Year { get; set; }
+ 
+}
