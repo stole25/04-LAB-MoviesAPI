9d925806c678c4b4c468015ce06ef56c7b4a5547
stole25 <119949884+stole25@users.noreply.github.com>
2024-12-10 18:25:05 +0100

    Revert "Bstanic"

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using MoviesAPI.Data;
-using MoviesAPI.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,10 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnString")
-                         ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
-builder.Services.AddScoped<MoviesService>();
 
 var app = builder.Build();
 
